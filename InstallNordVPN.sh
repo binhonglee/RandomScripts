ecda24180ad37a800d2f6dc2db7308650344d1d8
BinHong Lee <binhong@binhong.me>
2017-05-16 21:34:33 -0700

    Updated InstallNordVPN.sh to use relative path

--- InstallNordVPN.sh
+++ InstallNordVPN.sh
@@ -1,8 +1,9 @@
 sudo apt-get install openvpn
-cd /etc/openvpn
+pushd /etc/openvpn
 sudo wget https://nordvpn.com/api/files/zip
 sudo apt-get install ca-certificates
 sudo apt-get install unzip
 sudo unzip zip
 sudo rm zip
-sudo find *.ovpn > ~/RandomScripts/vpn_list
+pushd -0
+sudo find /etc/openvpn/*.ovpn > vpn_list
