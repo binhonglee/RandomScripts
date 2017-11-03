e3b28cf6e9d422dbbb403385a83a62a2f51a6b62
BinHong Lee <binhong@binhong.me>
2017-11-02 17:43:20 -0700

    Added support for Mac

--- updates.sh
+++ updates.sh
@@ -1,4 +1,9 @@
-sudo apt-get update
-sudo aptitude update
-sudo apt-get dist-upgrade
-sudo aptitude dist-upgrade
+if [ $(uname) == "Darwin" ]; then
+	brew update
+	brew upgrade
+else
+	sudo apt-get update
+	sudo aptitude update
+	sudo apt-get dist-upgrade
+	sudo aptitude dist-upgrade
+fi
